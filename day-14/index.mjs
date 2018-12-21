2699a1b0d77cfa4da08a10903f4c94515059b5cb
Tim Kurvers <tim@moonsphere.net>
2018-12-21 13:26:23 +0100

    Add solutions for day 14

--- day-14/index.mjs
+++ day-14/index.mjs
@@ -1,5 +1,62 @@
 #!/usr/bin/env node --experimental-modules --no-warnings
 
+import input from './input/example';
 import { day } from '../utils';
 
-day(14).part(1).solution(() => null);
+let recipes;
+let first;
+let second;
+
+const initialize = () => {
+  first = 0;
+  second = 1;
+  recipes = [3, 7];
+};
+
+const create = () => {
+  const sum = recipes[first] + recipes[second];
+  const numbers = sum.toString().split('').map(Number);
+  recipes.push(...numbers);
+
+  first = (first + 1 + recipes[first]) % recipes.length;
+  second = (second + 1 + recipes[second]) % recipes.length;
+};
+
+const match = (start, seek) => {
+  const { length } = seek;
+  for (let i = 0; i < length; ++i) {
+    if (recipes[start + i] !== seek[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+day(14).part(1).solution(() => {
+  initialize();
+
+  const required = 10;
+  while (recipes.length < input + required) {
+    create();
+  }
+  return recipes.slice(input, input + required).join('');
+});
+
+day(14).part(2).solution(() => {
+  initialize();
+
+  const seek = input.toString().split('').map(Number);
+  const { length } = seek;
+
+  while (true) {
+    create();
+
+    const offset = recipes.length - length;
+    if (match(offset, seek)) {
+      return offset;
+    }
+    if (match(offset - 1, seek)) {
+      return offset - 1;
+    }
+  }
+});
---
+++ day-14/input/index.mjs
@@ -0,0 +1,1 @@
+export default 637061;
