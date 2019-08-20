a115b7e4ed4d27b11dd6620ae59d147ae414fefa
Tim Kurvers <tim@moonsphere.net>
2019-08-20 22:22:34 +0200

    Add solutions for 2017 day 12

---
+++ 2017/12/Program.mjs
@@ -0,0 +1,52 @@
+class Program {
+  constructor(id) {
+    this.id = +id;
+    this.connections = new Set();
+  }
+
+  get group() {
+    const seen = new Set();
+    const todo = [this];
+
+    while (todo.length) {
+      const program = todo.pop();
+      if (!seen.has(program)) {
+        seen.add(program);
+        todo.push(...program.connections);
+      }
+    }
+
+    return seen;
+  }
+
+  connect(program) {
+    this.connections.add(program);
+    program.connections.add(this);
+  }
+
+  static from(input) {
+    const cache = new Map();
+
+    const lookup = (id) => {
+      let program = cache.get(id);
+      if (!program) {
+        program = new this(id);
+        cache.set(id, program);
+      }
+      return program;
+    };
+
+    input.split('\n').forEach((line) => {
+      const [id, rest] = line.split(' <-> ');
+      const connections = rest.split(', ').map(Number).map(lookup);
+      const program = lookup(+id);
+      for (const connection of connections) {
+        program.connect(connection);
+      }
+    });
+
+    return Array.from(cache.values());
+  }
+}
+
+export default Program;
---
+++ 2017/12/index.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+import { day } from '..';
+
+import Program from './Program';
+import examples from './input/examples';
+import puzzleInput from './input';
+
+day(12).part(1).test(examples).feed(puzzleInput).solution((input) => {
+  const programs = Program.from(input);
+  const program0 = programs.find(program => program.id === 0);
+  return program0.group.size;
+});
+
+day(12).part(2).test(examples).feed(puzzleInput).solution((input) => {
+  const programs = Program.from(input);
+
+  const hash = group => (
+    Array.from(group).map(program => program.id).sort().join('-')
+  );
+
+  const groups = programs.reduce((set, program) => {
+    const id = hash(program.group);
+    set.add(id);
+    return set;
+  }, new Set());
+
+  return groups.size;
+});
