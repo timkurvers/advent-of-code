7026b222cc2e5f8f65c9e91c00295a15ae144aa6
Tim Kurvers <tim@moonsphere.net>
2019-08-25 14:37:13 +0200

    Add solutions for 2017 day 10

---
+++ 2017/10/input/examples.mjs
@@ -0,0 +1,9 @@
+import { example } from '../../../utils';
+
+export default [
+  example('3, 4, 1, 5', 12),
+  example('', null, 'a2582a3a0e66e6e86e3812dcb672a272'),
+  example('AoC 2017', null, '33efeb34ea91902bb2f59c9920caa6cd'),
+  example('1,2,3', null, '3efbe78a8d82f29979031a4aa0b16a9d'),
+  example('1,2,4', null, '63960835bcdc130f0b66d7ff4f6a5a8e'),
+];
