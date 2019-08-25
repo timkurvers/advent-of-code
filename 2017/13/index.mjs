8383b14c65a7a03b985185f351ea0665f201f266
Tim Kurvers <tim@moonsphere.net>
2019-08-25 15:57:37 +0200

    Add solutions for 2017 day 13

---
+++ 2017/13/index.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+/* eslint-disable no-param-reassign */
+
+import { day } from '..';
+
+import examples from './input/examples';
+import puzzleInput from './input';
+
+const parse = input => input.split('\n').reduce((firewall, line) => {
+  const [depth, range] = line.split(': ').map(Number);
+  firewall[depth] = { depth, range };
+  return firewall;
+}, []);
+
+// See: https://en.wikipedia.org/wiki/Triangle_wave
+const position = (time, range) => (
+  range - Math.abs(range - (time % (2 * range)))
+);
+
+const probe = (firewall, { delay = 0 } = {}) => {
+  let caught = false;
+  let severity = 0;
+  let time = delay;
+  for (const layer of firewall) {
+    if (layer) {
+      const at = position(time, layer.range - 1);
+      if (at === 0) {
+        severity += layer.depth * layer.range;
+        caught = true;
+      }
+    }
+    ++time;
+  }
+  return { caught, severity };
+};
+
+day(13).part(1).test(examples).feed(puzzleInput).solution((input) => {
+  const firewall = parse(input);
+  return probe(firewall).severity;
+});
+
+day(13).part(2).test(examples).feed(puzzleInput).solution((input) => {
+  const firewall = parse(input);
+
+  let delay = 0;
+  let attempt;
+  do {
+    ++delay;
+    attempt = probe(firewall, { delay });
+  } while (attempt.caught);
+
+  return delay;
+});
---
+++ 2017/13/input/examples.mjs
@@ -0,0 +1,8 @@
+import { example } from '../../../utils';
+
+export default [
+  example(`0: 3
+1: 2
+4: 4
+6: 4`, 24, 10),
+];
