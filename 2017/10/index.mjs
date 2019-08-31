b919e4b9d05c6abc23df1ba066769c502863ae36
Tim Kurvers <tim@moonsphere.net>
2019-09-01 00:07:50 +0200

    Modularize knothash

--- 2017/10/index.mjs
+++ 2017/10/index.mjs
@@ -5,65 +5,20 @@ import { day } from '..';
 
 import examples from './input/examples';
 import puzzleInput from './input';
-
-const parse = (input, { asASCII = false } = {}) => {
-  if (asASCII) {
-    return input.split('').map(char => char.charCodeAt(0));
-  }
-  return input.split(',').map(Number);
-};
-
-const range = (isExample = false) => (
-  new Array(isExample ? 5 : 256).fill(0).map((_, index) => index)
-);
-
-const twist = (root, lengths, { rounds = 1 } = {}) => {
-  let position = root;
-  let skip = 0;
-  for (let round = 0; round < rounds; ++round) {
-    for (const length of lengths) {
-      let start = position;
-      let end = start.seek(length - 1);
-      for (let i = 0; i < length / 2; ++i) {
-        const temp = start.value;
-        start.value = end.value;
-        end.value = temp;
-
-        start = start.next;
-        end = end.prev;
-      }
-      position = position.seek(length + skip);
-      ++skip;
-    }
-  }
-};
-
-const densify = (sparse) => {
-  const dense = [];
-  for (let i = 0; i < 16; ++i) {
-    const slice = sparse.slice(i * 16, i * 16 + 16);
-    const block = slice.reduce((xor, current) => xor ^ current);
-    dense.push(block);
-  }
-  return dense;
-};
-
-const hexhash = dense => (
-  dense.map(number => number.toString(16).padStart(2, '0')).join('')
-);
+import {
+  createRange,
+  hexknothash,
+  parse,
+  twist,
+} from './knothash';
 
 day(10).part(1).test(examples).feed(puzzleInput).solution((input, isExample) => {
   const lengths = parse(input);
-  const root = CircularLinkedList.from(...range(isExample));
+  const root = CircularLinkedList.from(...createRange(isExample ? 5 : undefined));
   twist(root, lengths);
   return root.value * root.next.value;
 });
 
-day(10).part(2).test(examples).feed(puzzleInput).solution((input) => {
-  const lengths = parse(input, { asASCII: true });
-  lengths.push(17, 31, 73, 47, 23);
-  const root = CircularLinkedList.from(...range());
-  twist(root, lengths, { rounds: 64 });
-  const sparse = CircularLinkedList.toArray(root);
-  return hexhash(densify(sparse));
-});
+day(10).part(2).test(examples).feed(puzzleInput).solution(input => (
+  hexknothash(input)
+));
---
+++ 2017/10/knothash.mjs
@@ -0,0 +1,60 @@
+import { CircularLinkedList } from '../../utils';
+
+export const parse = (source, { asASCII = false } = {}) => {
+  if (asASCII) {
+    return source.split('').map(char => char.charCodeAt(0));
+  }
+  return source.split(',').map(Number);
+};
+
+export const createRange = (range = 256) => (
+  new Array(range).fill(0).map((_, index) => index)
+);
+
+export const twist = (root, lengths, { rounds = 1 } = {}) => {
+  let position = root;
+  let skip = 0;
+  for (let round = 0; round < rounds; ++round) {
+    for (const length of lengths) {
+      let start = position;
+      let end = start.seek(length - 1);
+      for (let i = 0; i < length / 2; ++i) {
+        const temp = start.value;
+        start.value = end.value;
+        end.value = temp;
+
+        start = start.next;
+        end = end.prev;
+      }
+      position = position.seek(length + skip);
+      ++skip;
+    }
+  }
+};
+
+const densify = (sparse) => {
+  const dense = [];
+  for (let i = 0; i < 16; ++i) {
+    const slice = sparse.slice(i * 16, i * 16 + 16);
+    const block = slice.reduce((xor, current) => xor ^ current);
+    dense.push(block);
+  }
+  return dense;
+};
+
+const hexhash = dense => (
+  dense.map(number => number.toString(16).padStart(2, '0')).join('')
+);
+
+export const knothash = (source, { range } = {}) => {
+  const lengths = parse(source, { asASCII: true });
+  lengths.push(17, 31, 73, 47, 23);
+  const root = CircularLinkedList.from(...createRange(range));
+  twist(root, lengths, { rounds: 64 });
+  const sparse = CircularLinkedList.toArray(root);
+  return densify(sparse);
+};
+
+export const hexknothash = (source, options) => (
+  hexhash(knothash(source, options))
+);
