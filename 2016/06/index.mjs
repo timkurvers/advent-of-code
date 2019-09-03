d04ef423671e42d1d850c31eb7d874d689a15e33
Tim Kurvers <tim@moonsphere.net>
2019-09-03 23:07:10 +0200

    Add solutions for 2016 day 6

---
+++ 2016/06/index.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+import { day } from '..';
+
+import examples from './input/examples';
+import puzzleInput from './input';
+
+const parse = input => input.split('\n');
+
+const recover = (words, { frequency } = {}) => {
+  let result = '';
+  const { length } = words[0];
+  const multiplier = frequency === 'most' ? 1 : -1;
+  for (let p = 0; p < length; ++p) {
+    const counts = {};
+    for (const word of words) {
+      const char = word[p];
+      counts[char] = (counts[char] || 0) + 1;
+    }
+
+    const top = Object.keys(counts).sort((a, b) => (
+      multiplier * (counts[b] - counts[a])
+    ));
+    result += top[0];
+  }
+  return result;
+};
+
+day(6).part(1).test(examples).feed(puzzleInput).solution(input => (
+  recover(parse(input), { frequency: 'most' })
+));
+
+day(6).part(2).test(examples).feed(puzzleInput).solution(input => (
+  recover(parse(input), { frequency: 'least' })
+));
---
+++ 2016/06/input/examples.mjs
@@ -0,0 +1,20 @@
+import { example } from '../../../utils';
+
+export default [
+  example(`eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar`, 'easter', 'advent'),
+];
