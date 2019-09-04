c57daa77eb524e0bf774a17f0930209f9c2b701e
Tim Kurvers <tim@moonsphere.net>
2019-09-04 21:02:34 +0200

    Add solutions for 2016 day 8

---
+++ 2016/08/index.mjs
@@ -0,0 +1,57 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+import { day } from '..';
+import { Grid, wrap } from '../../utils';
+
+import examples from './input/examples';
+import puzzleInput from './input';
+
+const INSTRUCTION_MATCHER = /(rect|row|column).+?(\d+).+?(\d+)/;
+
+const parse = input => input.split('\n').map((line) => {
+  const [, opcode, a, b] = line.match(INSTRUCTION_MATCHER);
+  return { opcode, a: +a, b: +b };
+});
+
+const swipe = (instructions) => {
+  const grid = new Grid();
+  grid.fill(0, 0, 49, 5, false);
+  for (const instruction of instructions) {
+    const { opcode, a, b } = instruction;
+    if (opcode === 'rect') {
+      for (let x = 0; x < a; ++x) {
+        for (let y = 0; y < b; ++y) {
+          grid.set(x, y, true);
+        }
+      }
+    } else if (opcode === 'row') {
+      const row = grid.row(a);
+      const { length } = row;
+      for (let i = 0; i < length; ++i) {
+        const index = wrap(i - b, length);
+        grid.set(i, a, row[index]);
+      }
+    } else if (opcode === 'column') {
+      const column = grid.column(a);
+      const { length } = column;
+      for (let i = 0; i < length; ++i) {
+        const index = wrap(i - b, length);
+        grid.set(a, i, column[index]);
+      }
+    }
+  }
+  return grid;
+};
+
+day(8).part(1).test(examples).feed(puzzleInput).solution((input) => {
+  const grid = swipe(parse(input));
+  return grid.filter(point => point.value).length;
+});
+
+day(8).part(2).test(examples).feed(puzzleInput).solution((input) => {
+  const grid = swipe(parse(input));
+  const output = grid.toString(point => (point && point.value ? '#' : '.'));
+  console.log();
+  console.log(output);
+  return '<see visually above>';
+});
---
+++ 2016/08/input/examples.mjs
@@ -0,0 +1,8 @@
+import { example } from '../../../utils';
+
+export default [
+  example(`rect 3x2
+rotate column x=1 by 1
+rotate row y=0 by 4
+rotate column x=1 by 1`, 6),
+];
