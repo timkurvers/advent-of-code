94e06805ffa4fc8fd1dc7aeaaf07255ae4e0aade
Tim Kurvers <tim@moonsphere.net>
2019-11-03 16:29:59 +0100

    Add solutions for 2015 day 11

---
+++ 2015/11/index.mjs
@@ -0,0 +1,71 @@
+#!/usr/bin/env node --experimental-modules --no-warnings
+
+import { day } from '..';
+
+import examples from './input/examples';
+import puzzleInput from './input';
+
+const A = 'a'.charCodeAt(0);
+const Z = 'z'.charCodeAt(0);
+
+const parse = input => input.split('').map(char => char.charCodeAt(0));
+const reconstruct = chars => String.fromCharCode(...chars);
+
+const ILLEGAL_CHARACTERS = parse('ilo');
+
+const validate = (chars) => {
+  const { length } = chars;
+
+  let straight = false;
+  let pairs = 0;
+
+  for (let i = 0; i < length; ++i) {
+    const a = chars[i];
+    const b = chars[i + 1];
+    const c = chars[i + 2];
+
+    if (ILLEGAL_CHARACTERS.includes(a)) {
+      return false;
+    }
+
+    if (a + 1 === b && a + 2 === c) {
+      straight = true;
+    }
+
+    if (a === b && a !== c) {
+      ++pairs;
+    }
+  }
+
+  return straight && pairs >= 2;
+};
+
+const step = (chars) => {
+  const next = [...chars];
+  const { length } = next;
+
+  for (let i = length - 1; i > 0; --i) {
+    if (next[i] !== Z) {
+      ++next[i];
+      break;
+    }
+    next[i] = A;
+  }
+  return next;
+};
+
+const next = (start) => {
+  let current = start;
+  do {
+    current = step(current);
+  } while (!validate(current));
+  return current;
+};
+
+day(11).part(1).test(examples).feed(puzzleInput).solution(input => (
+  reconstruct(next(parse(input)))
+));
+
+day(11).part(2).test(examples).feed(puzzleInput).solution(input => (
+  reconstruct(next(next(parse(input))))
+));
---
+++ 2015/11/input/examples.mjs
@@ -0,0 +1,6 @@
+import { example } from '../../../utils';
+
+export default [
+  example('abcdefgh', 'abcdffaa'),
+  example('ghijklmn', 'ghjaabcc'),
+];
