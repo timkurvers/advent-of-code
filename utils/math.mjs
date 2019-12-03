7598793b3c3be851b5108dff1f3dbdca8e1a550d
Tim Kurvers <tim@moonsphere.net>
2019-12-03 16:14:25 +0100

    Add reduceMinBy and reduceMaxBy

--- utils/math.mjs
+++ utils/math.mjs
@@ -5,3 +5,11 @@ export const sum = array => array.reduce((total, next) => total + next, 0);
 export const maxIndex = array => array.reduce((current, value, index) => (
   value > array[current] ? index : current
 ), 0);
+
+export const reduceMinBy = (array, prop) => array.reduce((candidate, other) => (
+  candidate[prop] > other[prop] ? other : candidate
+));
+
+export const reduceMaxBy = (array, prop) => array.reduce((candidate, other) => (
+  candidate[prop] < other[prop] ? other : candidate
+));
