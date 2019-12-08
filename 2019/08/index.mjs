0f80e1fa1ea387d163e426493f9e19f912833d47
Tim Kurvers <tim@moonsphere.net>
2019-12-08 16:22:40 +0100

    Add solutions for 2019 day 8

---
+++ 2019/08/index.mjs
@@ -0,0 +1,59 @@
+import { Grid, reduceMinBy, solution } from '../../utils';
+
+const Pixel = {
+  BLACK: '0',
+  WHITE: '1',
+  TRANSPARENT: '2',
+};
+
+const count = (str, char) => {
+  const matches = str.match(new RegExp(char, 'g'));
+  return matches ? matches.length : 0;
+};
+
+const layerify = (input, { width, height } = {}) => {
+  const { length } = input;
+  const pixelsPerLayer = width * height;
+  const layers = [];
+  for (let i = 0; i < length; i += pixelsPerLayer) {
+    const source = input.slice(i, i + pixelsPerLayer);
+    const zeroes = count(source, '0');
+    layers.push({ source, zeroes });
+  }
+  return layers;
+};
+
+const width = 25;
+const height = 6;
+
+export const partOne = solution((input, isExample) => {
+  const layers = layerify(input, {
+    width: isExample ? 3 : width,
+    height: isExample ? 2 : height,
+  });
+
+  const candidates = layers.filter(layer => layer.zeroes > 0);
+  const layer = reduceMinBy(candidates, 'zeroes');
+  return count(layer.source, '1') * count(layer.source, '2');
+});
+
+export const partTwo = solution((input) => {
+  const grid = new Grid();
+
+  const layers = layerify(input, { width, height });
+  for (const layer of layers.reverse()) {
+    const pixels = layer.source.split('');
+    for (const [index, pixel] of pixels.entries()) {
+      const x = index % width;
+      const y = (index / width) | 0;
+      if (pixel !== Pixel.TRANSPARENT) {
+        grid.set(x, y, pixel === Pixel.WHITE ? '#' : ' ');
+      }
+    }
+  }
+
+  const output = grid.toString();
+  console.log();
+  console.log(output);
+  return '<see visually above>';
+});
---
+++ 2019/08/input/examples.mjs
@@ -0,0 +1,5 @@
+import { example } from '../../../utils';
+
+export default [
+  example('123456789012', 1),
+];
