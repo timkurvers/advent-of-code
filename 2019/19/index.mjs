9e7e62403956993297c8e872bdd4e126a1420854
Tim Kurvers <tim@moonsphere.net>
2019-12-20 00:43:22 +0100

    Add solution for 2019 day 19 part 1

---
+++ 2019/19/index.mjs
@@ -0,0 +1,25 @@
+import IntcodeProgram from '../02/IntcodeProgram';
+import { Grid, solution } from '../../utils';
+
+const Type = {
+  BEAM: '#',
+  EMPTY: '.',
+};
+
+export const partOne = solution(async (input) => {
+  const grid = new Grid();
+
+  const program = IntcodeProgram.from(input);
+  for (let y = 0; y < 50; ++y) {
+    for (let x = 0; x < 50; ++x) {
+      program.reset();
+      program.input(x);
+      program.input(y);
+      await program.run();
+      const result = await program.output();
+      grid.set(x, y, result ? Type.BEAM : Type.EMPTY);
+    }
+  }
+
+  return grid.filter(point => point.value === Type.BEAM).length;
+});
