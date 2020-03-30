720c653222cf3656357ac31bc031b6aec5a51528
Tim Kurvers <tim@moonsphere.net>
2020-03-31 01:41:41 +0200

    Add solutions for 2015 day 9

---
+++ 2015/09/index.mjs
@@ -0,0 +1,46 @@
+import {
+  Graph,
+  permute,
+  reduceMaxBy,
+  reduceMinBy,
+  solution,
+} from '../../utils';
+
+const DISTANCE_MATCHER = /(\w+) to (\w+) = (\d+)/g;
+
+const parse = (input) => {
+  const graph = new Graph();
+  for (const match of input.matchAll(DISTANCE_MATCHER)) {
+    const [, a, b, distance] = match;
+    graph.link(a, b, { cost: +distance });
+  }
+  return graph;
+};
+
+const distanceFor = (route) => route.reduce((distance, current, index) => {
+  const next = route[index + 1];
+  if (next) {
+    const edge = current.edges.find((e) => e.to === next);
+    return distance + edge.cost;
+  }
+  return distance;
+}, 0);
+
+const plan = (input) => {
+  const graph = parse(input);
+  const routes = permute(graph.vertices);
+  return Array.from(routes).map((route) => ({
+    distance: distanceFor(route),
+    route,
+  }));
+};
+
+export const partOne = solution((input) => {
+  const routes = plan(input);
+  return reduceMinBy(routes, 'distance').distance;
+});
+
+export const partTwo = solution((input) => {
+  const routes = plan(input);
+  return reduceMaxBy(routes, 'distance').distance;
+});
---
+++ 2015/09/input/examples.mjs
@@ -0,0 +1,9 @@
+import { example, stripIndent } from '../../../utils';
+
+export const partOne = [
+  example(stripIndent`
+    London to Dublin = 464
+    London to Belfast = 518
+    Dublin to Belfast = 141
+  `, 605),
+];
