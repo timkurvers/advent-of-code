0bed7ace4b1c2f2e65fbb3698311c0913782a2d8
Tim Kurvers <tim@moonsphere.net>
2020-03-31 02:45:32 +0200

    Add solutions for 2015 day 17

---
+++ 2015/17/index.mjs
@@ -0,0 +1,30 @@
+import {
+  combine,
+  reduceMinBy,
+  solution,
+  sum,
+} from '../../utils';
+
+const parse = (input) => input.trim().split('\n').map(Number);
+
+const bruteforce = (containers, total, options = {}) => {
+  const combinations = Array.from(combine(containers, options));
+  const solutions = combinations.filter((combination) => (
+    sum(combination) === total
+  ));
+  return solutions;
+};
+
+export const partOne = solution.inefficient((input, { total = 150 } = {}) => {
+  const containers = parse(input);
+  const solutions = bruteforce(containers, total);
+  return solutions.length;
+});
+
+export const partTwo = solution.inefficient((input, { total = 150 } = {}) => {
+  const containers = parse(input);
+  let solutions = bruteforce(containers, total);
+  const smallest = reduceMinBy(solutions, 'length');
+  solutions = bruteforce(containers, total, { k: smallest.length });
+  return solutions.length;
+});
---
+++ 2015/17/input/examples.mjs
@@ -0,0 +1,21 @@
+import { example, stripIndent } from '../../../utils';
+
+export const partOne = [
+  example(stripIndent`
+    20
+    15
+    10
+    5
+    5
+  `, 4, { total: 25 }),
+];
+
+export const partTwo = [
+  example(stripIndent`
+    20
+    15
+    10
+    5
+    5
+  `, 3, { total: 25 }),
+];
