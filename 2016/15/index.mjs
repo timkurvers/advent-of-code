8eb8e543bb81f18d40f3a2504898bd6831dd5cca
Tim Kurvers <tim@moonsphere.net>
2020-04-06 22:32:51 +0200

    Add solutions for 2016 day 15

---
+++ 2016/15/index.mjs
@@ -0,0 +1,36 @@
+import { cast, solution, wrap } from '../../utils';
+
+const DISC_MATCHER = /Disc #(\d+) has (\d+) .+ position (\d+)./g;
+
+const parse = (input) => (
+  Array.from(input.matchAll(DISC_MATCHER)).map((match) => {
+    const [, id, positions, start] = match;
+    return { id: cast(id), positions: cast(positions), start: cast(start) };
+  })
+);
+
+const probe = (discs, t) => (
+  discs.every((disc, index) => {
+    const offset = wrap(disc.start + index + 1 + t, disc.positions);
+    return offset === 0;
+  })
+);
+
+const bruteforce = (discs) => {
+  for (let t = 0; ; ++t) {
+    if (probe(discs, t)) {
+      return t;
+    }
+  }
+};
+
+export const partOne = solution((input) => {
+  const discs = parse(input);
+  return bruteforce(discs);
+});
+
+export const partTwo = solution((input) => {
+  const discs = parse(input);
+  discs.push({ positions: 11, start: 0 });
+  return bruteforce(discs);
+});
---
+++ 2016/15/input/examples.mjs
@@ -0,0 +1,8 @@
+import { example, stripIndent } from '../../../utils';
+
+export const partOne = [
+  example(stripIndent`
+    Disc #1 has 5 positions; at time=0, it is at position 4.
+    Disc #2 has 2 positions; at time=0, it is at position 1.
+  `, 5),
+];
