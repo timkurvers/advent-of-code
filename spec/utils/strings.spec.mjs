948d0c76fb21281edc2d26b699c2631ee1919240
Tim Kurvers <tim@moonsphere.net>
2020-04-16 15:35:47 +0200

    Add string utilities specs

---
+++ spec/utils/strings.spec.mjs
@@ -0,0 +1,61 @@
+import {
+  toASCII,
+  fromASCII,
+  stripIndent,
+  humanize,
+  titleize,
+} from '../../utils';
+
+describe('string utilities', () => {
+  describe('toASCII', () => {
+    it('converts given string to an ASCII array of character codes', () => {
+      expect(toASCII('AoC')).toEqual([65, 111, 67]);
+    });
+  });
+
+  describe('fromASCII', () => {
+    it('converts given single ASCII character code to a string', () => {
+      expect(fromASCII(111)).toEqual('o');
+    });
+
+    it('converts given ASCII character codes to a string', () => {
+      expect(fromASCII([65, 111, 67])).toEqual('AoC');
+    });
+  });
+
+  describe('stripIndent', () => {
+    it('strips indentation from given string', () => {
+      const stripped = stripIndent`
+        {
+          // code block
+        }
+      `;
+      expect(stripped).toEqual('{\n  // code block\n}\n');
+    });
+  });
+
+  describe('humanize', () => {
+    it('transforms given string into a human-readable one', () => {
+      expect(humanize('partOne')).toEqual('Part one');
+      expect(humanize('åKjøre')).toEqual('Å kjøre');
+      expect(humanize('UTFConverter')).toEqual('Utf converter');
+    });
+
+    it('supports not capitalizing the result', () => {
+      const options = {
+        capitalize: false,
+      };
+      expect(humanize('partOne', options)).toEqual('part one');
+      expect(humanize('åKjøre', options)).toEqual('å kjøre');
+      expect(humanize('UTFConverter', options)).toEqual('utf converter');
+    });
+  });
+
+  describe('titleize', () => {
+    it('transforms given string into a capitalized human-readable one', () => {
+      expect(titleize('partOne')).toEqual('Part One');
+      expect(titleize('åKjøre')).toEqual('Å Kjøre');
+      expect(titleize('UTFConverter')).toEqual('Utf Converter');
+    });
+  });
+});
--- utils/strings.mjs
+++ utils/strings.mjs
@@ -19,9 +19,6 @@ export const stripIndent = (strings, ...params) => {
     `${string}${params[i] || ''}`
   )).join('');
 
-  // Strip leading / trailing new lines
-  source = source.replace(/^\s|\s$/, '');
-
   // See: https://github.com/zspecza/common-tags/blob/master/src/stripIndentTransformer/stripIndentTransformer.js
   const match = source.match(/^[^\S\n]*(?=\S)/gm);
   const indent = match && Math.min(...match.map((el) => el.length));
@@ -29,6 +26,10 @@ export const stripIndent = (strings, ...params) => {
     const regexp = new RegExp(`^.{${indent}}`, 'gm');
     source = source.replace(regexp, '');
   }
+
+  // Strip leading whitespace and trailing tabs/spaces
+  source = source.replace(/^\n+|[ \t]+$/g, '');
+
   return source;
 };
 
@@ -36,20 +37,18 @@ export const stripIndent = (strings, ...params) => {
 // See: https://api.rubyonrails.org/classes/ActiveSupport/Inflector.html
 
 export const humanize = (str, { capitalize = true } = {}) => {
-  str = str.replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2');
-  str = str.replace(/([a-z\d])([A-Z])/g, '$1 $2');
+  str = str.replace(/(\p{Upper}+)(\p{Upper}\p{Lower})/gu, '$1 $2');
+  str = str.replace(/([\p{Lower}\d])(\p{Upper})/gu, '$1 $2');
   str = str.replace(/[-_]/g, ' ');
   str = str.toLowerCase();
   if (capitalize) {
-    str = str.replace(/^[a-z]/, (char) => char.toUpperCase());
+    str = str.replace(/^\p{Lower}/u, (char) => char.toUpperCase());
   }
   return str;
 };
 
-export const titleize = (str, { allWords = true } = {}) => {
+export const titleize = (str) => {
   str = humanize(str);
-  if (allWords) {
-    str = str.replace(/ [a-z]/g, (chars) => chars.toUpperCase());
-  }
+  str = str.replace(/ \p{Lower}/gu, (char) => char.toUpperCase());
   return str;
 };
