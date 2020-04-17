79fadd2acb96129d855378bc228c6ffe5789f563
Tim Kurvers <tim@moonsphere.net>
2020-04-17 14:23:11 +0200

    Add navigation utilities specs

---
+++ spec/utils/navigation.spec.mjs
@@ -0,0 +1,79 @@
+import {
+  Orientation,
+  Rotation,
+  dx,
+  dy,
+  isHorizontalOrientation,
+  isVerticalOrientation,
+} from '../../utils';
+
+describe('navigation utilities', () => {
+  describe('Orientation', () => {
+    it('defines orientation constants', () => {
+      expect(Orientation.EAST).toBeDefined();
+      expect(Orientation.NORTH).toBeDefined();
+      expect(Orientation.WEST).toBeDefined();
+      expect(Orientation.SOUTH).toBeDefined();
+
+      expect(Orientation.UP).toBeDefined();
+      expect(Orientation.DOWN).toBeDefined();
+      expect(Orientation.LEFT).toBeDefined();
+      expect(Orientation.RIGHT).toBeDefined();
+    });
+  });
+
+  describe('Rotation', () => {
+    it('defines rotation constants', () => {
+      expect(Rotation.TURN_LEFT).toBeDefined();
+      expect(Rotation.TURN_RIGHT).toBeDefined();
+      expect(Rotation.TURN_AROUND).toBeDefined();
+      expect(Rotation.NONE).toBeDefined();
+    });
+  });
+
+  describe('dx()', () => {
+    it('returns horizontal translation for given orientation', () => {
+      expect(dx(Orientation.EAST)).toEqual(1);
+      expect(dx(Orientation.NORTH)).toEqual(0);
+      expect(dx(Orientation.WEST)).toEqual(-1);
+      expect(dx(Orientation.SOUTH)).toEqual(-0);
+
+      expect(dx(Orientation.UP)).toEqual(-0);
+      expect(dx(Orientation.DOWN)).toEqual(0);
+      expect(dx(Orientation.LEFT)).toEqual(-1);
+      expect(dx(Orientation.RIGHT)).toEqual(1);
+    });
+  });
+
+  describe('dy()', () => {
+    it('returns vertical translation for given orientation', () => {
+      expect(dy(Orientation.EAST)).toEqual(0);
+      expect(dy(Orientation.NORTH)).toEqual(1);
+      expect(dy(Orientation.WEST)).toEqual(0);
+      expect(dy(Orientation.SOUTH)).toEqual(-1);
+
+      expect(dy(Orientation.UP)).toEqual(-1);
+      expect(dy(Orientation.DOWN)).toEqual(1);
+      expect(dy(Orientation.LEFT)).toEqual(0);
+      expect(dy(Orientation.RIGHT)).toEqual(0);
+    });
+  });
+
+  describe('isHorizontalOrientation()', () => {
+    it('returns whether given orientation is horizontal', () => {
+      expect(isHorizontalOrientation(Orientation.UP)).toBe(false);
+      expect(isHorizontalOrientation(Orientation.DOWN)).toBe(false);
+      expect(isHorizontalOrientation(Orientation.LEFT)).toBe(true);
+      expect(isHorizontalOrientation(Orientation.RIGHT)).toBe(true);
+    });
+  });
+
+  describe('isVerticalOrientation()', () => {
+    it('returns whether given orientation is horizontal', () => {
+      expect(isVerticalOrientation(Orientation.UP)).toBe(true);
+      expect(isVerticalOrientation(Orientation.DOWN)).toBe(true);
+      expect(isVerticalOrientation(Orientation.LEFT)).toBe(false);
+      expect(isVerticalOrientation(Orientation.RIGHT)).toBe(false);
+    });
+  });
+});
--- utils/navigation.mjs
+++ utils/navigation.mjs
@@ -1,11 +1,18 @@
 export const Orientation = {
+  EAST: Math.PI * 0,
+  NORTH: Math.PI * 0.5,
+  WEST: Math.PI * 1,
+  SOUTH: Math.PI * 1.5,
+
+  // UP / DOWN are flipped to ensure proper vertical movements in grids
   UP: Math.PI * 1.5,
   DOWN: Math.PI * 0.5,
-  LEFT: Math.PI,
-  RIGHT: 0,
+  LEFT: Math.PI * 1,
+  RIGHT: Math.PI * 0,
 };
 
 export const Rotation = {
+  // TURN_LEFT / TURN_RIGHT are flipped to ensure proper vertical movements in grids
   TURN_LEFT: -Math.PI * 0.5,
   TURN_RIGHT: Math.PI * 0.5,
   TURN_AROUND: Math.PI,
