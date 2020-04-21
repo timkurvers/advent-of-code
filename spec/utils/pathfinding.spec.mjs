14b0974bc3492268f920e7f347c4bda79a2f560d
Tim Kurvers <tim@moonsphere.net>
2020-04-21 18:00:13 +0200

    Add dummy specs for pathfinding utilities

---
+++ spec/utils/pathfinding.spec.mjs
@@ -0,0 +1,11 @@
+import { astar, bfs } from '../../utils';
+
+describe('pathfinding utilities', () => {
+  describe('astar()', () => {
+    it.todo('pathfinds');
+  });
+
+  describe('bfs()', () => {
+    it.todo('pathfinds');
+  });
+});
