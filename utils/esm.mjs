2f9889ac46330f2ef5a47dfccd61c8d382645aaa
Tim Kurvers <tim@moonsphere.net>
2020-04-21 17:35:55 +0200

    Add specs for ESM utilities

--- utils/esm.mjs
+++ utils/esm.mjs
@@ -1,5 +1,5 @@
-import { dirname as pdirname } from 'path';
+import { basename as pbasename, dirname as pdirname } from 'path';
 import { fileURLToPath } from 'url';
 
+export const basename = (url) => pbasename(fileURLToPath(url));
 export const dirname = (url) => pdirname(fileURLToPath(url));
-export const filename = (url) => fileURLToPath(url);
