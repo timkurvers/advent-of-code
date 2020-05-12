1f8c8f98e9167d986b272949bbcb7fb37a0cc2dc
Tim Kurvers <tim@moonsphere.net>
2020-05-12 14:24:47 +0200

    Add solutions for 2015 day 20

---
+++ 2015/20/index.mjs
@@ -0,0 +1,34 @@
+import { solution } from '../../utils';
+
+const distribute = ({ numHouses, perHouse, maxDeliveries = Infinity }) => {
+  const houses = new Array(Math.ceil(numHouses)).fill(0);
+  for (let elf = 1; elf < numHouses; ++elf) {
+    for (
+      let house = elf, delivery = 0;
+      delivery < maxDeliveries && house < numHouses;
+      ++delivery, house += elf
+    ) {
+      houses[house] += elf * perHouse;
+    }
+  }
+  return houses;
+};
+
+export const partOne = solution.inefficient((target) => {
+  const perHouse = 10;
+  const houses = distribute({
+    numHouses: target / perHouse,
+    perHouse,
+  });
+  return houses.findIndex((house) => house >= target);
+});
+
+export const partTwo = solution((target, { maxDeliveries = 50 }) => {
+  const perHouse = 11;
+  const houses = distribute({
+    numHouses: target / perHouse,
+    perHouse,
+    maxDeliveries,
+  });
+  return houses.findIndex((house) => house >= target);
+});
---
+++ 2015/20/input/index.mjs
@@ -0,0 +1,1 @@
+export default 36000000;
