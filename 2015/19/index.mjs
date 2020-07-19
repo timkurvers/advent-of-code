78a0983228380c979468041364c0c4b8a8f44048
Tim Kurvers <tim@moonsphere.net>
2020-07-19 02:01:39 +0200

    Add solution for 2015 day 19 part 2

--- 2015/19/index.mjs
+++ 2015/19/index.mjs
@@ -1,4 +1,6 @@
-import { solution } from '../../utils';
+/* eslint-disable no-cond-assign, no-loop-func */
+
+import { shuffle, solution } from '../../utils';
 
 const REPLACEMENT_MATCHER = /(\w+) => (\w+)/g;
 
@@ -28,8 +30,45 @@ const permute = (string, transitions) => {
   return replacements;
 };
 
+const fabricate = (medicine, transitions) => {
+  const root = 'e';
+  let current = medicine;
+  let steps = 0;
+  while (current !== root) {
+    const antistuck = current;
+    for (const { from, to } of transitions) {
+      if (!current.includes(to)) {
+        continue;
+      }
+      const next = current.replace(to, from);
+      if (!next.includes(root) || next === root) {
+        current = next;
+        ++steps;
+      }
+    }
+    if (antistuck === current) {
+      return null;
+    }
+  }
+  return steps;
+};
+
 export const partOne = solution((input) => {
   const { medicine, transitions } = parse(input);
   const replacements = permute(medicine, transitions);
   return new Set(replacements).size;
+});
+
+export const partTwo = solution((input) => {
+  const { medicine, transitions } = parse(input);
+
+  let steps;
+  while (true) {
+    // Attempt to fabricate the medicine
+    if (steps = fabricate(medicine, transitions)) {
+      return steps;
+    }
+    // Shuffle list of transitions on failure and retry
+    shuffle(transitions);
+  }
 });
--- utils/data-structures/index.mjs
+++ utils/data-structures/index.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+
 export { default as BinaryHeap } from './BinaryHeap';
 export { default as Cache } from './Cache';
 export { default as CircularLinkedList, CircularLinkedListNode } from './CircularLinkedList';
@@ -6,6 +8,16 @@ export { default as Graph, GraphEdge, GraphVertex } from './Graph';
 export { default as Grid, GridPoint } from './Grid';
 export { default as PriorityQueue } from './PriorityQueue';
 export { default as Queue } from './Queue';
+
+// Shuffles given array in-place
+// See: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+export const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 export const range = ({ start = 0, end, length = end + 1 - start }) => (
   new Array(length).fill(0).map((_, index) => start + index)
