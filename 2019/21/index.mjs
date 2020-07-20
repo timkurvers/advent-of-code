fe59c3090187352d924a06f7f14d445147ac4400
Tim Kurvers <tim@moonsphere.net>
2020-07-21 01:58:37 +0200

    Add solution for 2019 day 21 part 1

--- 2019/21/index.mjs
+++ 2019/21/index.mjs
@@ -1,15 +1,34 @@
 import IntcodeProgram from '../02/IntcodeProgram';
-import { solution, stripIndent, toASCII } from '../../utils';
+import {
+  fromASCII,
+  solution,
+  stripIndent,
+  toASCII,
+} from '../../utils';
 
-export const partOne = solution(async (input) => {
+const springscript = async (input, instructions) => {
   const program = IntcodeProgram.from(input);
-
-  const instructions = stripIndent`
-    NOT A J
-    WALK
-  `;
-
   program.inputs = toASCII(instructions);
 
   await program.run();
-});
+
+  const { outputs } = program;
+  const result = outputs[outputs.length - 1];
+  if (result > 255) {
+    return result;
+  }
+  console.log(fromASCII(outputs));
+  return null;
+};
+
+export const partOne = solution((input) => (
+  // Pseudo-code:
+  // if ((third === hole && fourth === ground) || first === hole)
+  springscript(input, stripIndent`
+    NOT C J
+    AND D J
+    NOT A T
+    OR T J
+    WALK
+  `)
+));
