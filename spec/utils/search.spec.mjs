cbf51bb154794545f380e3cdfad1a9ab78f3e288
Tim Kurvers <tim@moonsphere.net>
2020-07-23 02:26:36 +0200

    Correct bisect spec

--- spec/utils/search.spec.mjs
+++ spec/utils/search.spec.mjs
@@ -2,13 +2,13 @@ import { bisect, range } from '../../utils';
 
 describe('search utilities', () => {
   describe('bisect()', () => {
-    it('finds lowest value where given condition is truthy', () => {
+    it('finds lowest value where given condition is truthy', async () => {
       const start = 0;
       const end = 10;
       const indices = range({ start, end });
 
       for (const index of indices) {
-        expect(bisect({
+        expect(await bisect({
           lower: start,
           upper: end,
           until: (x) => x >= index,
