cf0548d52d1f79b4d39e8cde9f6a0b704eef31a3
Tim Kurvers <tim@moonsphere.net>
2021-11-28 23:27:52 +0100

    docs(rust): add TODO to only run requested challenges

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -7,6 +7,7 @@ use crate::utils::challenges::prelude::*;
 preload_challenges!(into CHALLENGES);
 
 fn main() {
+    // TODO: Only run requested challenges
     for argument in env::args() {
         println!("{}", argument)
     }
