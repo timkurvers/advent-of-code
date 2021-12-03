c2d5ed549dbcb5df4ceaae3135a9c765e4b11326
Tim Kurvers <tim@moonsphere.net>
2021-12-03 19:38:43 +0100

    feat(js): add solutions for 2021 day 2

---
+++ js/src/2021/02/index.mjs
@@ -0,0 +1,47 @@
+import { solution } from '../../utils';
+
+const parse = (input) => (
+  input.trim().split('\n').map((line) => {
+    const [direction, value] = line.split(' ');
+    return { direction, value: Number(value) };
+  })
+);
+
+const navigate = (commands, { rtfm = false } = {}) => {
+  let position = 0;
+  let depth = 0;
+  let aim = 0;
+
+  for (const { direction, value } of commands) {
+    if (direction === 'forward') {
+      position += value;
+      if (rtfm) {
+        depth += aim * value;
+      }
+    } else if (direction === 'down') {
+      if (rtfm) {
+        aim += value;
+      } else {
+        depth += value;
+      }
+    } else if (direction === 'up') {
+      if (rtfm) {
+        aim -= value;
+      } else {
+        depth -= value;
+      }
+    }
+  }
+
+  return position * depth;
+};
+
+export const partOne = solution((input) => {
+  const commands = parse(input);
+  return navigate(commands);
+});
+
+export const partTwo = solution((input) => {
+  const commands = parse(input);
+  return navigate(commands, { rtfm: true });
+});
