6eb6421d378eb1d0737e575767d6cdf43ac1a4eb
Tim Kurvers <tim@moonsphere.net>
2021-12-06 17:29:21 +0100

    feat(rust): add solutions for 2021 day 6

---
+++ rust/src/2021/06.rs
@@ -0,0 +1,40 @@
+use crate::utils::challenges::prelude::*;
+
+type Fish = usize;
+
+fn parse(input: &PuzzleInput) -> Vec<Fish> {
+    input.trim().split(',').map(|s| s.parse().unwrap()).collect()
+}
+
+fn count(fishies: Vec<Fish>, days: u64) -> u64 {
+    const SLOTS: usize = 9;
+    let mut lifetimes = [0; SLOTS];
+
+    for fish in fishies {
+        lifetimes[fish] += 1;
+    }
+
+    for _ in 0..days {
+        let birthing = lifetimes[0];
+        for i in 0..SLOTS - 1 {
+            lifetimes[i] = lifetimes[i + 1];
+        }
+        lifetimes[6] += birthing;
+        lifetimes[8] = birthing;
+    }
+    lifetimes.iter().sum()
+}
+
+fn part_one(input: &PuzzleInput, _args: &RawPuzzleArgs) -> Solution {
+    let fishies = parse(input);
+    let total = count(fishies, 80);
+    Answer(total)
+}
+
+fn part_two(input: &PuzzleInput, _args: &RawPuzzleArgs) -> Solution {
+    let fishies = parse(input);
+    let total = count(fishies, 256);
+    Answer(total)
+}
+
+solve!(part_one, part_two);
