e4c90548b421f70edb4c14234c0664a0736a9fe5
Tim Kurvers <tim@moonsphere.net>
2021-12-09 10:16:28 +0100

    feat(js): add solutions for 2021 day 9

---
+++ js/src/2021/09/index.mjs
@@ -0,0 +1,43 @@
+/* eslint-disable no-cond-assign */
+
+import {
+  Grid, bfs, solution, sum,
+} from '../../utils';
+
+const parse = (input) => Grid.from(input.trim(), { cast: Number });
+
+export const partOne = solution((input) => {
+  const grid = parse(input);
+  return sum(grid.map((point) => {
+    const { value } = point;
+    const isHigherLevel = (neighbor) => neighbor.value > value;
+    if (point.adjacentNeighbors.every(isHigherLevel)) {
+      return 1 + value;
+    }
+    return 0;
+  }));
+});
+
+export const partTwo = solution((input) => {
+  const grid = parse(input);
+
+  const basins = [];
+
+  const visited = new Set();
+  const isBasinPoint = (point) => point.value !== 9;
+  const isUnprocessed = (point) => isBasinPoint(point) && !visited.has(point);
+
+  let current = null;
+  while (current = grid.find(isUnprocessed)) {
+    const { visited: basin } = bfs(current, null, {
+      neighborsFor: (point) => point.adjacentNeighbors.filter(isBasinPoint),
+    });
+    for (const point of basin) {
+      visited.add(point);
+    }
+    basins.push(basin);
+  }
+
+  const top3 = basins.sort((a, b) => b.size - a.size).slice(0, 3);
+  return top3.reduce((acc, basin) => acc * basin.size, 1);
+});
