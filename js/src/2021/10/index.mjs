dcdbdd5f2f880234167af0f36c93d569a2777a57
Tim Kurvers <tim@moonsphere.net>
2021-12-10 10:14:32 +0100

    feat(js): add solutions for 2021 day 10

---
+++ js/src/2021/10/index.mjs
@@ -0,0 +1,64 @@
+import { solution } from '../../utils';
+
+const parse = (input) => (
+  input.trim().split('\n')
+);
+
+const OPEN_CLOSE_PAIRS = {
+  '(': ')', '[': ']', '{': '}', '<': '>',
+};
+const CLOSE_OPEN_PAIRS = {
+  ')': '(', ']': '[', '}': '{', '>': '<',
+};
+const INVALID_SCORE = {
+  ')': 3, ']': 57, '}': 1197, '>': 25137,
+};
+const FIX_SCORE = {
+  ')': 1, ']': 2, '}': 3, '>': 4,
+};
+
+const validate = (line) => {
+  const stack = [];
+  for (const char of line) {
+    if (char in CLOSE_OPEN_PAIRS) {
+      const last = stack.pop();
+      if (CLOSE_OPEN_PAIRS[char] !== last) {
+        return [false, char, stack];
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+  return [true, null, stack];
+};
+
+export const partOne = solution((input) => {
+  const lines = parse(input);
+
+  let points = 0;
+  for (const line of lines) {
+    const [valid, syntax] = validate(line);
+    if (!valid) {
+      points += INVALID_SCORE[syntax];
+    }
+  }
+  return points;
+});
+
+export const partTwo = solution((input) => {
+  const lines = parse(input);
+
+  const candidates = [];
+  for (const line of lines) {
+    const [valid,, stack] = validate(line);
+    if (valid) {
+      const points = stack.reverse().reduce((acc, char) => (
+        acc * 5 + FIX_SCORE[OPEN_CLOSE_PAIRS[char]]
+      ), 0);
+      candidates.push(points);
+    }
+  }
+
+  const sorted = candidates.sort((a, b) => b - a);
+  return sorted[Math.floor(sorted.length / 2)];
+});
