8180a835273eaec14c15c6d8b8fb615fb671024a
Tim Kurvers <tim@moonsphere.net>
2021-12-11 16:57:47 +0100

    feat(js): add solutions for 2021 day 11

---
+++ js/src/2021/11/index.mjs
@@ -0,0 +1,55 @@
+/* eslint-disable no-cond-assign, no-param-reassign */
+
+import { Grid, solution } from '../../utils';
+
+const parse = (input) => Grid.from(input, { cast: Number });
+
+const round = (grid) => {
+  const flashed = new Set();
+  const frontier = [];
+
+  const increase = (point) => {
+    if (flashed.has(point)) {
+      return;
+    }
+    point.value += 1;
+    if (point.value > 9) {
+      point.value = 0;
+      frontier.push(...point.neighbors);
+      flashed.add(point);
+    }
+  };
+
+  for (const point of grid.points) {
+    increase(point);
+  }
+
+  let current = null;
+  while (current = frontier.pop()) {
+    increase(current);
+  }
+
+  return flashed.size;
+};
+
+export const partOne = solution((input, { steps = 100 } = {}) => {
+  const grid = parse(input);
+
+  let flashes = 0;
+  for (let step = 1; step <= steps; ++step) {
+    flashes += round(grid);
+  }
+  return flashes;
+});
+
+export const partTwo = solution((input) => {
+  const grid = parse(input);
+  const { length } = grid.points;
+
+  for (let step = 1; ; ++step) {
+    const flashes = round(grid);
+    if (flashes === length) {
+      return step;
+    }
+  }
+});
