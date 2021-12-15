9867f619c5054aebc7766d2c6b08564abc083433
Tim Kurvers <tim@moonsphere.net>
2021-12-15 07:15:22 +0100

    feat(js): add solutions for 2021 day 15

---
+++ js/src/2021/15/index.mjs
@@ -0,0 +1,56 @@
+import { Grid, astar, solution } from '../../utils';
+
+const parse = (input) => {
+  const grid = Grid.from(input, { cast: Number });
+  return grid;
+};
+
+export const partOne = solution((input) => {
+  const grid = parse(input);
+
+  const start = grid.getPoint(0, 0);
+  const goal = grid.getPoint(grid.maxX, grid.maxY);
+
+  const result = astar(start, goal, {
+    neighborsFor: (point) => point.adjacentNeighbors,
+    cost: (_current, next) => next.value,
+  });
+  return result.score;
+});
+
+export const partTwo = solution((input) => {
+  const grid = parse(input);
+  const { width, height } = grid;
+
+  for (const point of grid.points) {
+    const { x, y } = point;
+    let base = point.value;
+    for (let ytile = 0; ytile < 5; ++ytile) {
+      if (ytile !== 0) {
+        ++base;
+        if (base > 9) {
+          base = 1;
+        }
+      }
+      let value = base;
+      for (let xtile = 0; xtile < 5; ++xtile) {
+        if (xtile !== 0) {
+          ++value;
+          if (value > 9) {
+            value = 1;
+          }
+        }
+        grid.set(xtile * width + x, ytile * height + y, value);
+      }
+    }
+  }
+
+  const start = grid.getPoint(0, 0);
+  const goal = grid.getPoint(grid.maxX, grid.maxY);
+
+  const result = astar(start, goal, {
+    neighborsFor: (point) => point.adjacentNeighbors,
+    cost: (_current, next) => next.value,
+  });
+  return result.score;
+});
