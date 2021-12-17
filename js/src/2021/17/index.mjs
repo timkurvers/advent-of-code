906007d4769a52501e65a01f8c005cc85aaaf135
Tim Kurvers <tim@moonsphere.net>
2021-12-17 16:19:38 +0100

    refactor(js): speed up solutions for 2021 day 17

--- js/src/2021/17/index.mjs
+++ js/src/2021/17/index.mjs
@@ -9,7 +9,11 @@ const parse = (input) => {
   };
 };
 
-const lob = (sdx, sdy, target) => {
+const launch = (sdx, sdy, target) => {
+  const {
+    x1, x2, y1, y2,
+  } = target;
+
   let dx = sdx;
   let dy = sdy;
   let x = 0;
@@ -17,52 +21,50 @@ const lob = (sdx, sdy, target) => {
   let height = -Infinity;
 
   // Brute-force all the things!
-  let step = 4000;
-  while (step) {
+  while (true) {
     if (y > height) {
       height = y;
     }
-    if (x >= target.x1 && x <= target.x2 && y >= target.y1 && y <= target.y2) {
+    if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
       return { sdx, sdy, height };
     }
     x += dx;
     y += dy;
     dy -= 1;
     dx += -Math.sign(dx);
-    --step;
+
+    if (
+      (y < y1 && dy < 0) // Will never get in Y-range
+      || (x < x1 && dx < 0) // Will never get in X-range
+      || (dx === 0 && (x < x1 || x > x2)) // Yeeting straight up into space
+    ) {
+      break;
+    }
   }
   return null;
 };
 
-export const partOne = solution.inefficient((input) => {
-  const target = parse(input);
-
+const bruteforce = (target, start = -400, end = 400) => {
   const candidates = [];
-  for (const dx of range({ length: 500 })) {
-    for (const dy of range({ length: 500 })) {
-      if (dx === 0 || dy === 0) {
-        continue;
-      }
-      const result = lob(dx, dy, target);
+  for (const dx of range({ start, end })) {
+    for (const dy of range({ start, end })) {
+      const result = launch(dx, dy, target);
       if (result) {
         candidates.push(result);
       }
     }
   }
+  return candidates;
+};
+
+export const partOne = solution((input) => {
+  const target = parse(input);
+  const candidates = bruteforce(target);
   return reduceMaxBy(candidates, 'height').height;
 });
 
-export const partTwo = solution.inefficient((input) => {
+export const partTwo = solution((input) => {
   const target = parse(input);
-
-  const candidates = [];
-  for (const dx of range({ start: -400, end: 400 })) {
-    for (const dy of range({ start: -400, end: 400 })) {
-      const result = lob(dx, dy, target);
-      if (result) {
-        candidates.push(result);
-      }
-    }
-  }
+  const candidates = bruteforce(target);
   return candidates.length;
 });
