160d78cc23827fcc9e2cf510656364ab4c9a81f4
Tim Kurvers <tim@moonsphere.net>
2022-12-01 14:12:43 +0100

    feat(js): add solutions for 2022 day 1

---
+++ js/src/2022/01/index.js
@@ -0,0 +1,19 @@
+import { cast, solution, sum } from '../../utils';
+
+const parse = (input) => (
+  input.trim().split('\n\n').map((inventory) => (
+    sum(inventory.split('\n').map(cast))
+  ))
+);
+
+export const partOne = solution((input) => {
+  const inventories = parse(input);
+  return Math.max(...inventories);
+});
+
+export const partTwo = solution((input) => {
+  const inventories = parse(input);
+  inventories.sort((a, b) => b - a);
+  const top3 = inventories.slice(0, 3);
+  return sum(top3);
+});
