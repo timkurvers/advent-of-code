f1529baf26f4be07d192262dea6bac855c77543c
Tim Kurvers <tim@moonsphere.net>
2022-12-04 11:11:15 +0100

    feat(js): add solutions for 2022 day 4

---
+++ js/src/2022/04/index.js
@@ -0,0 +1,36 @@
+import { solution } from '../../utils';
+
+const parse = (input) => (
+  input.trim().split('\n').map((line) => (
+    line.split(',').map((section) => {
+      const [start, end] = section.split('-').map(Number);
+      return { start, end };
+    })
+  ))
+);
+
+const isContaining = (pair) => {
+  const [one, two] = pair;
+  return (
+    (one.start <= two.start && one.end >= two.end)
+    || (two.start <= one.start && two.end >= one.end)
+  );
+};
+
+const isOverlapping = (pair) => {
+  const [one, two] = pair;
+  return (
+    (one.start >= two.start && one.start <= two.end)
+    || (two.start >= one.start && two.start <= one.end)
+  );
+};
+
+export const partOne = solution((input) => {
+  const pairs = parse(input);
+  return pairs.filter(isContaining).length;
+});
+
+export const partTwo = solution((input) => {
+  const pairs = parse(input);
+  return pairs.filter(isOverlapping).length;
+});
