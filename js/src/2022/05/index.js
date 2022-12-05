1a9498c2d16d4cf36f6cf09264f0e6f5e7c3954c
Tim Kurvers <tim@moonsphere.net>
2022-12-05 16:40:37 +0100

    feat(js): add solutions for 2022 day 5

---
+++ js/src/2022/05/index.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-multi-assign */
+
+import { solution } from '../../utils';
+
+const CRATE_MATCHER = /\[([A-Z])\]/g;
+const MOVE_MATCHER = /move (?<count>\d+) from (?<from>\d+) to (?<to>\d+)/g;
+
+const parse = (input) => {
+  const [first, second] = input.split('\n\n');
+
+  const stacks = [];
+  for (const line of first.split('\n')) {
+    for (const match of line.matchAll(CRATE_MATCHER)) {
+      const index = match.index / 4;
+      const stack = (stacks[index] ||= []);
+      stack.unshift(match[1]);
+    }
+  }
+
+  const procedure = Array.from(second.matchAll(MOVE_MATCHER)).map((match) => ({
+    count: +match.groups.count,
+    from: +match.groups.from - 1,
+    to: +match.groups.to - 1,
+  }));
+
+  return { stacks, procedure };
+};
+
+const serialize = (stacks) => stacks.reduce((str, stack) => (
+  str + stack[stack.length - 1]
+), '');
+
+const step = (stacks, move, { maintainOrder = false } = {}) => {
+  const slice = stacks[move.from].splice(-move.count, move.count);
+  if (!maintainOrder) {
+    slice.reverse();
+  }
+  stacks[move.to].push(...slice);
+};
+
+export const partOne = solution((input) => {
+  const { stacks, procedure } = parse(input);
+  for (const move of procedure) {
+    step(stacks, move);
+  }
+  return serialize(stacks);
+});
+
+export const partTwo = solution((input) => {
+  const { stacks, procedure } = parse(input);
+  for (const move of procedure) {
+    step(stacks, move, { maintainOrder: true });
+  }
+  return serialize(stacks);
+});
