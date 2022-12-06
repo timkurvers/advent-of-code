6c3ac838f37affe30b6168575958d7a20e07178f
Tim Kurvers <tim@moonsphere.net>
2022-12-06 06:24:56 +0100

    feat(js): add solutions for 2022 day 6

---
+++ js/src/2022/06/index.js
@@ -0,0 +1,25 @@
+import { solution } from '../../utils';
+
+const parse = (input) => input.trim().split('');
+
+// Returns start/end indices of first unique sequence of given length
+const findUniqueSequence = (stream, { length }) => {
+  for (let i = 0; i < stream.length; ++i) {
+    const slice = stream.slice(i, i + length);
+    const set = new Set(slice);
+    if (set.size === length) {
+      return { start: i, end: i + length };
+    }
+  }
+  return null;
+};
+
+export const partOne = solution((input) => {
+  const stream = parse(input);
+  return findUniqueSequence(stream, { length: 4 }).end;
+});
+
+export const partTwo = solution((input) => {
+  const stream = parse(input);
+  return findUniqueSequence(stream, { length: 14 }).end;
+});
