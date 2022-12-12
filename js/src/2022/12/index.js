8c9da1bc17b54a5b995d97759af479ccbfc1871c
Tim Kurvers <tim@moonsphere.net>
2022-12-12 06:45:59 +0100

    feat(js): add solutions for 2022 day 12

---
+++ js/src/2022/12/index.js
@@ -0,0 +1,48 @@
+import { Grid, astar, solution } from '../../utils';
+
+const parse = (input) => {
+  const grid = Grid.from(input);
+  const start = grid.find((p) => p.value === 'S');
+  const goal = grid.find((p) => p.value === 'E');
+  start.value = 'a';
+  goal.value = 'z';
+  return { grid, start, goal };
+};
+
+const elevationAt = (letter) => letter.charCodeAt(0) - 96;
+
+const canMove = (from, to) => {
+  const cost = elevationAt(to.value) - elevationAt(from.value);
+  return cost <= 1;
+};
+
+const navigate = (start, goal) => (
+  astar(start, goal, {
+    neighborsFor: (current) => {
+      const neighbors = current.adjacentNeighbors;
+      return neighbors.filter((next) => canMove(current, next));
+    },
+  })
+);
+
+export const partOne = solution((input) => {
+  const { start, goal } = parse(input);
+  const result = navigate(start, goal);
+  return result.score;
+});
+
+export const partTwo = solution((input) => {
+  const { grid, goal } = parse(input);
+  const candidates = grid.filter((p) => p.value === 'a');
+
+  let best;
+  for (const start of candidates) {
+    const result = navigate(start, goal);
+
+    // Some candidate squares do not connect to the goal
+    if (result && (!best || result.score < best.score)) {
+      best = result;
+    }
+  }
+  return best.score;
+});
