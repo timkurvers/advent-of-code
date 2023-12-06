9920b11f7c98dba208a098c2e8613429c07e0b7b
Tim Kurvers <tim@moonsphere.net>
2023-12-06 17:34:34 +0100

    feat(js): add solutions for 2023 day 6

---
+++ js/src/2023/06/index.js
@@ -0,0 +1,34 @@
+import { cast, multiply, solution } from '../../utils';
+
+const parse = (input) => {
+  const nrs = input.trim().match(/\d+/g).map(cast);
+  const races = [];
+  for (let i = 0; i < nrs.length / 2; ++i) {
+    races.push({ time: nrs[i], record: nrs[i + nrs.length / 2] });
+  }
+  return races;
+};
+
+const simulate = (race) => {
+  const { time, record } = race;
+
+  let wins = 0;
+  for (let t = 1; t < time; ++t) {
+    const speed = t;
+    const distance = (race.time - t) * speed;
+    if (distance > record) {
+      ++wins;
+    }
+  }
+  return wins;
+};
+
+export const partOne = solution((input) => {
+  const races = parse(input);
+  return multiply(races.map(simulate));
+});
+
+export const partTwo = solution((input) => {
+  const races = parse(input.replaceAll(' ', ''));
+  return multiply(races.map(simulate));
+});
