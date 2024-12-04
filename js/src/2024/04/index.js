bd3ba79b440276b4a8eb3fcae31ad8c7ace0bbc4
Tim Kurvers <tim@moonsphere.net>
2024-12-04 12:30:03 +0100

    feat(js): add solutions for 2024 day 4

---
+++ js/src/2024/04/index.js
@@ -0,0 +1,82 @@
+import { Grid, solution } from '../../utils/index.js';
+
+const parse = (input) => Grid.from(input);
+
+const DIRECTIONS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
+
+// Keep diagonals only
+const CROSS_PATTERN_DIRECTIONS = DIRECTIONS.filter(([dx, dy]) => dx !== 0 && dy !== 0);
+
+// Whether given letters are all encountered when walking the grid from starting point in the dx/dy direction
+const matches = (letters, { grid, point, dx, dy }) =>
+  letters.every((letter, index) => {
+    const value = grid.get(point.x + dx * index, point.y + dy * index);
+    return value === letter;
+  });
+
+const search = (grid, word) => {
+  const letters = word.split('');
+  let count = 0;
+  for (const point of grid) {
+    for (const [dx, dy] of DIRECTIONS) {
+      count += +matches(letters, { grid, point, dx, dy });
+    }
+  }
+  return count;
+};
+
+const searchCrossPattern = (grid, word) => {
+  const letters = word.split('');
+  const half = (letters.length / 2) | 0;
+
+  const seen = new Set();
+  const hash = (point, dx, dy) => `${point.x}:${point.y}:${dx}:${dy}`;
+
+  let count = 0;
+  for (const point of grid) {
+    for (const [dx, dy] of CROSS_PATTERN_DIRECTIONS) {
+      if (seen.has(hash(point, dx, dy))) {
+        continue;
+      }
+
+      // Verify initial diagonal pattern
+      if (!matches(letters, { grid, point, dx, dy })) {
+        continue;
+      }
+
+      // In addition to a horizontally flipped one
+      let cross = grid.getPoint(point.x + (half + 1) * dx, point.y);
+      if (cross && matches(letters, { grid, point: cross, dx: -dx, dy })) {
+        seen.add(hash(cross, -dx, dy));
+        count += 1;
+      }
+
+      // Or a vertically flipped one
+      cross = grid.getPoint(point.x, point.y + (half + 1) * dy);
+      if (cross && matches(letters, { grid, point: cross, dx, dy: -dy })) {
+        seen.add(hash(cross, dx, -dy));
+        count += 1;
+      }
+    }
+  }
+  return count;
+};
+
+export const partOne = solution((input) => {
+  const grid = parse(input);
+  return search(grid, 'XMAS');
+});
+
+export const partTwo = solution((input) => {
+  const grid = parse(input);
+  return searchCrossPattern(grid, 'MAS');
+});
